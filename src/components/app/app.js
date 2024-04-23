5fbfbd9baed89ef71f28ce5b8939c1a5c6017552
Dmytro <solonar96@gmail.com>
2024-04-23 12:57:54 +0300

    Added id to employee-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
 	const data = [
-		{name: 'Thomas J.', salary: 800, increase: true},
-		{name: 'John D. R.', salary: 2000, increase: false},
-		{name: 'Abraham L.', salary: 5000, increase: false}
+		{name: 'Thomas J.', salary: 800, increase: true, id: 1},
+		{name: 'John D. R.', salary: 2000, increase: false, id: 2},
+		{name: 'Abraham L.', salary: 5000, increase: false, id: 3}
 	]
 
 	return (
